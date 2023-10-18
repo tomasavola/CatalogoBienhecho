a600eaea49ec224a9cfe1fe2ae9fe5381a2a0c82
tomasavola <108414862+tomasavola@users.noreply.github.com>
2023-10-18 20:35:09 -0300

    Update CartProduct.js

--- TP CATALOGO BIEN HECHO/src/components/CartProduct.js
+++ TP CATALOGO BIEN HECHO/src/components/CartProduct.js
@@ -1,1 +1,21 @@
+import React, {useContext} from 'react'
+import deleteIcon from '../images/deleteIcon.png'
+import { CartContext } from '../context/CartContext'
 
+export default function CartProduct({ product }) {
+
+  let {deleteProduct} = useContext(CartContext);
+
+  return (
+    <div className='productCheckout'>
+      <div>
+        <h1>{product.title}</h1>
+        <h2 style={{ color: 'rgb(245, 181, 53)' }} >${product.price}</h2>
+      </div>
+      <img src={product.images[1]} className="logo" alt="" />
+      <button onClick={() => deleteProduct(product)} className="deleteContainer">
+        <img src={deleteIcon} className="deleteIcon" alt="" />
+      </button>
+    </div>
+  )
+}
